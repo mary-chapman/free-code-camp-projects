644710c4b2ef5f989baa2321a5d7497f73ce4299
mary-chapman <mary.chapman.design@gmail.com>
2016-10-03 16:04:27 -0500

    quotes updated

--- p2_quote/quotes.js
+++ p2_quote/quotes.js
@@ -2,23 +2,27 @@ console.log("hi");
 
 
 
-$.getJSON("https://gist.githubusercontent.com/b1nary/ea8fff806095bcedacce/raw/6e6de20d7514b93dd69b149289264997b49459dd/enterpreneur-quotes.json", function(data){
-	
+$.getJSON("https://raw.githubusercontent.com/fortrabbit/quotes/master/quotes.json", function(data){
+
 	var q;
 	var author;
-	
+
 	$("#qt").on("click", function() {
 		var newContent = '';
 		var tweetIcon = '';
 		var rand = Math.floor(Math.random() * data.length);
 		q = data[rand].text;
-		author = data[rand].from;
-		newContent += "<p>" + q + '"' + "<br>" + "-" + author + "</p>";
+		var w = q.replace("\"", "X");
+		console.log(w);
+		author = data[rand].author;
+		newContent += "<p>" + q + "<br>" + "-" + author + "</p>";
 		tweetIcon += "<img src='twitterIcon.png' width=40>";
 		$("#quote").html(newContent);
 		$("#pic").html(tweetIcon);
-		
+
 	});
+
+
 
 	$('#pic').on('click', function() {
 		var url = "https://twitter.com/intent/tweet?text=" + q + '-' + author;
@@ -26,8 +30,6 @@ $.getJSON("https://gist.githubusercontent.com/b1nary/ea8fff806095bcedacce/raw/6e
 	});
 
 
-	
-	
+
+
 });
-
-
