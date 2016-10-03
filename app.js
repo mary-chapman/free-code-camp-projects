cdb8546d92edc72d3240b36672f1ed22b6f22d5f
mary-chapman <mary.chapman.design@gmail.com>
2016-10-03 15:18:52 -0500

    github buttons

---
+++ app.js
@@ -0,0 +1,16 @@
+$(".ghLink1").mouseover(function() {
+    $(".gh_hover1").css("display", "block");
+    $(".gh_btn1").css("display", "none");
+});
+$(".ghLink1").mouseout(function() {
+    $(".gh_hover1").css("display", "none" );
+    $(".gh_btn1").css("display", "block");
+});
+$(".ghLink2").mouseover(function() {
+    $(".gh_hover2").css("display", "block");
+    $(".gh_btn2").css("display", "none");
+});
+$(".ghLink2").mouseout(function() {
+    $(".gh_hover2").css("display", "none" );
+    $(".gh_btn2").css("display", "block");
+});
