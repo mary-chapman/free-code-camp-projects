461234816cc3dce08ec233109948ce91c1ca78f6
mary-chapman <mary.chapman.design@gmail.com>
2016-10-05 12:15:52 -0500

    calculator thumbnail added

--- app.js
+++ app.js
@@ -30,3 +30,11 @@ $(".ghLink4").mouseout(function() {
     $(".gh_hover4").css("display", "none" );
     $(".gh_btn4").css("display", "block");
 });
+$(".ghLink5").mouseover(function() {
+    $(".gh_hover5").css("display", "block");
+    $(".gh_btn5").css("display", "none");
+});
+$(".ghLink5").mouseout(function() {
+    $(".gh_hover5").css("display", "none" );
+    $(".gh_btn5").css("display", "block");
+});
