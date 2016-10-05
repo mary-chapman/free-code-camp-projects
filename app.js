870fa6d1378e29ae2ea465b79bfd2c0bbce09b8b
mary-chapman <mary.chapman.design@gmail.com>
2016-10-05 10:09:54 -0500

    weather added

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ $(".ghLink2").mouseout(function() {
     $(".gh_hover2").css("display", "none" );
     $(".gh_btn2").css("display", "block");
 });
+$(".ghLink3").mouseover(function() {
+    $(".gh_hover3").css("display", "block");
+    $(".gh_btn3").css("display", "none");
+});
+$(".ghLink2").mouseout(function() {
+    $(".gh_hover3").css("display", "none" );
+    $(".gh_btn3").css("display", "block");
+});
