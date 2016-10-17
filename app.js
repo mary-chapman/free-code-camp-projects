be45587b1884997718f9e199945904897d380032
mary-chapman <mary.chapman.design@gmail.com>
2016-10-17 18:23:36 -0500

    design changes

--- app.js
+++ app.js
@@ -38,3 +38,35 @@ $(".ghLink5").mouseout(function() {
     $(".gh_hover5").css("display", "none" );
     $(".gh_btn5").css("display", "block");
 });
+$(".ghLink6").mouseover(function() {
+    $(".gh_hover6").css("display", "block");
+    $(".gh_btn6").css("display", "none");
+});
+$(".ghLink6").mouseout(function() {
+    $(".gh_hover6").css("display", "none" );
+    $(".gh_btn6").css("display", "block");
+});
+$(".ghLink7").mouseover(function() {
+    $(".gh_hover7").css("display", "block");
+    $(".gh_btn7").css("display", "none");
+});
+$(".ghLink7").mouseout(function() {
+    $(".gh_hover7").css("display", "none" );
+    $(".gh_btn7").css("display", "block");
+});
+$(".ghLink8").mouseover(function() {
+    $(".gh_hover8").css("display", "block");
+    $(".gh_btn8").css("display", "none");
+});
+$(".ghLink8").mouseout(function() {
+    $(".gh_hover8").css("display", "none" );
+    $(".gh_btn8").css("display", "block");
+});
+$(".ghLink9").mouseover(function() {
+    $(".gh_hover9").css("display", "block");
+    $(".gh_btn9").css("display", "none");
+});
+$(".ghLink9").mouseout(function() {
+    $(".gh_hover9").css("display", "none" );
+    $(".gh_btn9").css("display", "block");
+});
